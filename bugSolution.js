c44848c3108338841afdd6c941127a4be2980256
BugHunter-X <askzenai@gmail.com>
2025-02-01 07:02:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  const parsedId = parseInt(userId);
+  if (isNaN(parsedId)) {
+    return res.status(400).send('Invalid user ID');
+  }
+  const user = users.find(user => user.id === parsedId);
+  if (!user) {
+    return res.status(404).send('User not found');
+  } else {
+    return res.send(user);
+  }
+});
